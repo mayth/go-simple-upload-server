9c072b18c59e74f7ec2f74ae5644b849f9895401
mayth <chimeaquas@hotmail.com>
2016-06-29 04:07:11 +0900

    initial implement

---
+++ server.go
@@ -0,0 +1,197 @@
+package main
+
+import (
+	"crypto/sha1"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path"
+	"regexp"
+
+	"github.com/Sirupsen/logrus"
+)
+
+// Server represents a simple-upload server.
+type Server struct {
+	DocumentRoot string
+	// MaxUploadSize limits the size of the uploaded content, specified with "byte".
+	MaxUploadSize int64
+	SecureToken   string
+}
+
+// NewServer creates a new simple-upload server.
+func NewServer(documentRoot string, maxUploadSize int64, token string) Server {
+	return Server{
+		DocumentRoot:  documentRoot,
+		MaxUploadSize: maxUploadSize,
+		SecureToken:   token,
+	}
+}
+
+func (s Server) handleGet(w http.ResponseWriter, r *http.Request) {
+	re := regexp.MustCompile(`^/files/([^/]+)$`)
+	if !re.MatchString(r.URL.Path) {
+		w.WriteHeader(http.StatusNotFound)
+		writeError(w, fmt.Errorf("\"%s\" is not found", r.URL.Path))
+		return
+	}
+	http.StripPrefix("/files/", http.FileServer(http.Dir(s.DocumentRoot))).ServeHTTP(w, r)
+}
+
+func (s Server) handlePost(w http.ResponseWriter, r *http.Request) {
+	srcFile, info, err := r.FormFile("file")
+	if err != nil {
+		logger.WithError(err).Error("failed to acquire the uploaded content")
+		w.WriteHeader(http.StatusInternalServerError)
+		writeError(w, err)
+		return
+	}
+	defer srcFile.Close()
+	logger.Debug(info)
+	size, err := getSize(srcFile)
+	if err != nil {
+		logger.WithError(err).Error("failed to get the size of the uploaded content")
+		w.WriteHeader(http.StatusInternalServerError)
+		writeError(w, err)
+		return
+	}
+	if size > s.MaxUploadSize {
+		logger.WithField("size", size).Info("file size exceeded")
+		w.WriteHeader(http.StatusRequestEntityTooLarge)
+		writeError(w, errors.New("uploaded file size exceeds the limit"))
+		return
+	}
+
+	body, err := ioutil.ReadAll(srcFile)
+	if err != nil {
+		logger.WithError(err).Error("failed to read the uploaded content")
+		w.WriteHeader(http.StatusInternalServerError)
+		writeError(w, err)
+		return
+	}
+	filename := info.Filename
+	if filename == "" {
+		filename = fmt.Sprintf("%x", sha1.Sum(body))
+	}
+
+	dstPath := path.Join(s.DocumentRoot, filename)
+	dstFile, err := os.OpenFile(dstPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
+	if err != nil {
+		logger.WithError(err).WithField("path", dstPath).Error("failed to open the file")
+		w.WriteHeader(http.StatusInternalServerError)
+		writeError(w, err)
+		return
+	}
+	defer dstFile.Close()
+	if written, err := dstFile.Write(body); err != nil {
+		logger.WithError(err).WithField("path", dstPath).Error("failed to write the content")
+		w.WriteHeader(http.StatusInternalServerError)
+		writeError(w, err)
+		return
+	} else if int64(written) != size {
+		logger.WithFields(logrus.Fields{
+			"size":    size,
+			"written": written,
+		}).Error("uploaded file size and written size differ")
+		w.WriteHeader(http.StatusInternalServerError)
+		writeError(w, fmt.Errorf("the size of uploaded content is %d, but %d bytes written", size, written))
+	}
+	logger.WithFields(logrus.Fields{
+		"path": dstPath,
+		"size": size,
+	}).Info("file uploaded by POST")
+	w.WriteHeader(http.StatusOK)
+	writeSuccess(w, dstPath)
+}
+
+func (s Server) handlePut(w http.ResponseWriter, r *http.Request) {
+	re := regexp.MustCompile(`^/files/([^/]+)$`)
+	matches := re.FindStringSubmatch(r.URL.Path)
+	if matches == nil {
+		logger.WithField("path", r.URL.Path).Info("invalid path")
+		w.WriteHeader(http.StatusNotFound)
+		writeError(w, fmt.Errorf("\"%s\" is not found", r.URL.Path))
+		return
+	}
+	targetPath := path.Join(s.DocumentRoot, matches[1])
+	file, err := os.OpenFile(targetPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
+	if err != nil {
+		logger.WithError(err).WithField("path", targetPath).Error("failed to open the file")
+		w.WriteHeader(http.StatusInternalServerError)
+		writeError(w, err)
+		return
+	}
+	defer file.Close()
+	defer r.Body.Close()
+	srcFile, info, err := r.FormFile("file")
+	if err != nil {
+		logger.WithError(err).WithField("path", targetPath).Error("failed to acquire the uploaded content")
+		w.WriteHeader(http.StatusInternalServerError)
+		writeError(w, err)
+		return
+	}
+	defer srcFile.Close()
+	// dump headers for the file
+	logger.Debug(info.Header)
+
+	size, err := getSize(srcFile)
+	if err != nil {
+		logger.WithError(err).WithField("path", targetPath).Error("failed to get the size of the uploaded content")
+		w.WriteHeader(http.StatusInternalServerError)
+		writeError(w, err)
+		return
+	}
+	if size > s.MaxUploadSize {
+		logger.WithFields(logrus.Fields{
+			"path": targetPath,
+			"size": size,
+		}).Info("file size exceeded")
+		w.WriteHeader(http.StatusRequestEntityTooLarge)
+		writeError(w, errors.New("uploaded file size exceeds the limit"))
+		return
+	}
+
+	n, err := io.Copy(file, srcFile)
+	if err != nil {
+		logger.WithError(err).WithField("path", targetPath).Error("failed to write body to the file")
+		w.WriteHeader(http.StatusInternalServerError)
+		writeError(w, err)
+		return
+	}
+	logger.WithFields(logrus.Fields{
+		"path": targetPath,
+		"size": n,
+	}).Info("file uploaded by PUT")
+	w.WriteHeader(http.StatusOK)
+	writeSuccess(w, targetPath)
+}
+
+func (s Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// first, try to get the token from the query strings
+	token := r.URL.Query().Get("token")
+	// if token is not found, check the form parameter.
+	if token == "" {
+		token = r.Form.Get("token")
+	}
+	if token != s.SecureToken {
+		w.WriteHeader(http.StatusUnauthorized)
+		writeError(w, fmt.Errorf("authentication required"))
+		return
+	}
+
+	switch r.Method {
+	case http.MethodGet, http.MethodHead:
+		s.handleGet(w, r)
+	case http.MethodPost:
+		s.handlePost(w, r)
+	case http.MethodPut:
+		s.handlePut(w, r)
+	default:
+		w.Header().Add("Allow", "GET,HEAD,POST,PUT")
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		writeError(w, fmt.Errorf("method \"%s\" is not allowed", r.Method))
+	}
+}
---
+++ simple_upload_server.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+
+	"crypto/rand"
+
+	"github.com/Sirupsen/logrus"
+)
+
+var logger *logrus.Logger
+
+func run(args []string) int {
+	bindAddress := flag.String("ip", "0.0.0.0", "IP address to bind")
+	listenPort := flag.Int("port", 25478, "port number to listen on")
+	// 5,242,880 bytes == 5 MiB
+	maxUploadSize := flag.Int64("upload_limit", 5242880, "max size of uploaded file (byte)")
+	tokenFlag := flag.String("token", "", "specify the security token (it is automatically generated if empty)")
+	logLevelFlag := flag.String("loglevel", "info", "logging level")
+	flag.Parse()
+	serverRoot := flag.Arg(0)
+	if len(serverRoot) == 0 {
+		flag.Usage()
+		return 2
+	}
+	if logLevel, err := logrus.ParseLevel(*logLevelFlag); err != nil {
+		logrus.WithError(err).Error("failed to parse logging level, so set to default")
+	} else {
+		logger.Level = logLevel
+	}
+	token := *tokenFlag
+	if token == "" {
+		count := 10
+		b := make([]byte, count)
+		if _, err := rand.Read(b); err != nil {
+			logger.WithError(err).Fatal("could not generate token")
+			return 1
+		}
+		token = fmt.Sprintf("%x", b)
+		logger.WithField("token", token).Warn("token generated")
+	}
+	logger.WithFields(logrus.Fields{
+		"ip":           *bindAddress,
+		"port":         *listenPort,
+		"token":        token,
+		"upload_limit": *maxUploadSize,
+		"root":         serverRoot,
+	}).Info("start listening")
+	server := NewServer(serverRoot, *maxUploadSize, token)
+	http.Handle("/upload", server)
+	http.Handle("/files/", server)
+	http.ListenAndServe(fmt.Sprintf("%s:%d", *bindAddress, *listenPort), nil)
+	return 0
+}
+
+func main() {
+	logger = logrus.New()
+	logger.Info("starting up simple-upload-server")
+
+	result := run(os.Args)
+	os.Exit(result)
+}
